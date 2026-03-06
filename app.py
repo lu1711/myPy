ee8b64935a756bcb05bb35ac846ad6a925422548
lucia <132006022+lu1711@users.noreply.github.com>
2026-03-06 12:02:18 +0000

    Add files via upload
    gestor de tarefas

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tarefas.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Tarefa(db.Model):
+    __tablename__ = "tarefas"
+    id = db.Column(db.Integer, primary_key=True)
+    conteudo = db.Column(db.String(200))
+    feita = db.Column(db.Boolean)
+
+with app.app_context():
+    db.create_all()
+    import os
+    print("Base de dados criada em:", os.path.abspath("tarefas.db"))
+
+
+@app.route('/')
+def home():
+    todas_as_tarefas = Tarefa.query.all()
+    return render_template("index.html", lista_de_tarefas=todas_as_tarefas)
+
+
+@app.route('/criar-tarefa', methods=['POST'])
+def criar():
+    tarefa = Tarefa(conteudo=request.form['conteudo_tarefa'], feita=False)
+    db.session.add(tarefa)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/eliminar-tarefa/<id>')
+def eliminar(id):
+    tarefa = Tarefa.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/tarefa-feita/<id>')
+def feita(id):
+    tarefa = Tarefa.query.filter_by(id=int(id)).first()
+    tarefa.feita = not tarefa.feita
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

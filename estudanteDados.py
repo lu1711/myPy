a663ab6c823d1ab99db67090e86940b35c823035
lucia <132006022+lu1711@users.noreply.github.com>
2026-03-06 12:04:58 +0000

    Add files via upload

---
+++ estudanteDados.py
@@ -0,0 +1,12 @@
+def estudante():
+    nome = str(input("Insira o seu nome:\n"))
+    idade = int(input("Insira a sua idade:\n"))
+    nota_matematica = float(input("Introduza a sua nota de matemática até à primeira casa decimal:\n"))
+
+    print("O seu nome é ", nome )
+    print("Idade:", idade)
+    print("A sua nota a matemática foi", nota_matematica)
+
+estudante()
+
+

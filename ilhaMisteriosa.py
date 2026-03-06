fefa8f0cabb3627e0ca6babf303d9016f44c542d
lucia <132006022+lu1711@users.noreply.github.com>
2026-03-06 12:04:40 +0000

    Add files via upload

---
+++ ilhaMisteriosa.py
@@ -0,0 +1,25 @@
+def ilhaMisteriosa():
+    print ("Acabou de chegar a uma ilha desconhecida, ouve-se o som de alguns animais.")
+    print ("Na praia um barco destruído, reza a lenda que trazia um tesouro...")
+    print ("Vamos resolver este mistério?")
+    print ("Vais ter 3 opções para começar:")
+    print ("A - Entrar na Selva" )
+    print ("B - Cruzar a ponte")
+    print ("C - Entrar na caverna")
+    a = str("A")
+    b = str("B")
+    c = str("C")
+    t = str("T")
+    m = str("M")
+    r = str("R")
+
+
+    caminho = str(input("Insere a letra correspondente ao caminho que queres seguir:"))
+    
+
+
+ilhaMisteriosa()
+
+
+
+
